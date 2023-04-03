1a671c3a152e4de4ffc149d16e1dc5e0325034b7
José Augusto <jmalengo@brq.com>
2023-04-03 09:24:19 -0300

    Test

---
+++ BusBookingTests/BusBookingTests.swift
@@ -0,0 +1,25 @@
+//
+//  BusBookingTests.swift
+//  BusBookingTests
+//
+//  Created by Jose Augusto on 03/04/23.
+//
+
+import XCTest
+
+final class BusBookingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        let name = "Jose"
+        XCTAssertEqual("Jose", name)
+    }
+
+}
