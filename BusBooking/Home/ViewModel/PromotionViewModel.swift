95527bdd7aec947983565f27dd08697bca229bb0
Jose Augusto <jose.malengo@hotmail.com>
2023-05-07 16:30:30 -0700

    configure

---
+++ BusBooking/Home/ViewModel/PromotionViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PromotionViewModel.swift
+//  BusBooking
+//
+//  Created by Jose Malengo on 07/05/23.
+//
+
+import Foundation
--- Modules/Package.swift
+++ Modules/Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "RemoteConfig",
+            name: "RemoteConfig", 
             dependencies: [.product(name: "FirebaseRemoteConfig", package: "Firebase")]),
         .testTarget(
             name: "RemoteConfigTests",
